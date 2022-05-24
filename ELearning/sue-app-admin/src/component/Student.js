8a6093597c3102528cb9a8b909058757885f44b1
Ajinkya <ajinkyad@winsoftech.com>
2022-05-24 10:57:56 +0530

    added table, graphql in admin app

--- ELearning/sue-app-admin/src/component/Student.js
+++ ELearning/sue-app-admin/src/component/Student.js
@@ -1,5 +1,73 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { ALL_STUDENTS } from '../graphql/StudentQueries';
+import {useQuery} from "@apollo/client";
+import { Skeleton } from '@mui/material';
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+// const rows = [
+//   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+//   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+//   createData('Eclair', 262, 16.0, 24, 6.0),
+//   createData('Cupcake', 305, 3.7, 67, 4.3),
+//   createData('Gingerbread', 356, 16.0, 49, 3.9),
+// ];
+
+let rows = [];
+
 export function Student() {
-    return(
-        <span> Student </span>
-    )
+
+    const { loading, error, data } = useQuery(ALL_STUDENTS);
+
+    if (data == undefined) {
+        return (
+            <>
+                <Skeleton variant="text" />
+                <Skeleton variant="circular" width={40} height={40} />
+                <Skeleton variant="rectangular" width={210} height={118} />
+            </>
+            
+        );
+    } else {
+        rows = [...data.users]
+
+    }
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="center">Name</TableCell>
+            <TableCell align="center">Email</TableCell>
+            <TableCell align="center">Address</TableCell>
+            <TableCell align="center">Phone</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell align="center">
+              {row.firstName} {row.lastName}
+              </TableCell>
+              <TableCell align="center">{row.contact.email}</TableCell>
+              <TableCell align="center">{row.contact.address}</TableCell>
+              <TableCell align="center">{row.contact.mobileNumber}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
---
+++ ELearning/sue-app-admin/src/graphql/StudentQueries.js
@@ -0,0 +1,41 @@
+import { gql } from "@apollo/client";
+
+export const GET_USER = gql `
+    query getUserByUserName($username: String) {
+        userByUsername(username: $username) {
+            firstName
+                middleName
+                lastName
+                age
+                gender
+                type
+                contact {
+                    userId,
+                    email,
+                    id,
+                    address,
+                    mobileNumber
+                }
+        }
+    }
+`;
+
+export const ALL_STUDENTS = gql `
+    query getAllUsers {
+        users {
+            firstName
+                middleName
+                lastName
+                age
+                gender
+                type
+                contact {
+                    userId,
+                    email,
+                    id,
+                    address,
+                    mobileNumber
+                }
+        }
+    }
+`
